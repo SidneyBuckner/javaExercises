7b913a87bf475b70e9c52edd0728451b037c3f5c
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-04 11:35:55 -0500

    completed first Java exercises

--- helloWorld/src/Hello.java
+++ helloWorld/src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World! My name is Sidney and I am a Software Engineer learning Java!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(1000 - myTotal);
     }
 }
