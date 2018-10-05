bd72244720d8c2ee1e81bbe3e7195148e21bc94f
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-05 13:56:38 -0500

    Completed whole number data type exercises to classify the four main data types to allocate spaces in memory for whole numbers

---
+++ byteShortInt/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Interger
+        int myMinvalue = -214783648;
+        int myMaxvalue = 214783648;
+        int myTotal = (myMinvalue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        byte myFirstByte = -128;
+        byte myNewbyteValue = (byte)(myFirstByte/2);
+        System.out.println("myNewbyteValue = " + myNewbyteValue);
+        
+        short myFirstShort = -32768;
+        long myFirstLong = 100;
+
+
+
+    }
+}
