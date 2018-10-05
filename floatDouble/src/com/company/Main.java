0e6786b74a4ccead9c4cad0e07ba97ba41d923a8
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-05 15:27:52 -0500

    Completed decimal number data type exercises to understand the difference in values between double and float

---
+++ floatDouble/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myIntValue = 5;
+	float myFloatValue = (float)(5.4);
+	double myDoubleValue =5d;
+
+	System.out.println("The integer is valued at: " + myIntValue);
+	System.out.println("The double is valued at: " + myDoubleValue);
+	System.out.println("The float is valued at: " + myFloatValue);
+    }
+}
