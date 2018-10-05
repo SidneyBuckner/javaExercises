41611011f7df5126bb3fa99381eddb3a6eb5ff7f
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-05 16:00:25 -0500

    complete next set of decimal data type exercises

--- floatDouble/src/com/company/Main.java
+++ floatDouble/src/com/company/Main.java
@@ -3,12 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int myIntValue = 5;
-	float myFloatValue = (float)(5.4);
-	double myDoubleValue =5d;
+	int myIntValue = 5/3;
+	float myFloatValue = 5f/3f;
+	double myDoubleValue =5.0 / 3.0;
 
 	System.out.println("The integer is valued at: " + myIntValue);
 	System.out.println("The double is valued at: " + myDoubleValue);
 	System.out.println("The float is valued at: " + myFloatValue);
+
+	//convert a given number of pounds to kilos
+    //create a var to store the number of pounds
+    //calculate the number of kilos from the pounds given and store in a variable
+    //Print out the result
+
+    double pounds = 58;
+    double kilo = pounds * 0.45359237;
+        System.out.println("The amount of kilos is: " + kilo);
+
     }
 }
