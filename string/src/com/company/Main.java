e065d6458fcba0fdae6108b18e882933df84dfec
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-06 12:57:37 -0500

    added and completed string directpry and exercises'

---
+++ string/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 8 main Primitive Data Types
+    // int - Whole Numbers(holds -2.147b to 2.147b)
+    // byte - whole numbers(holds -128 to 127)
+    // short - Whole Numbers(holds -32768 to 32767)
+    // long - Biigest allocation for whole numbers( holds -2**63 to 2**63)
+    // char - Holds one character or unicode ''
+    // boolean - True or false: var is the question, value is true or false
+    // float - decimals f
+    // double - most precises for decimals d
+
+        String myIntro = "Im Sidney and Im a Software Engineer. Nice to meet you!";
+        System.out.println(myIntro);
+
+
+
+    }
+}
