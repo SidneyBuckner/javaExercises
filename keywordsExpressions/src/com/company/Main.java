390f7a92a577ec336ade98fc8e2e4dfb889d83cd
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-08 11:10:15 -0500

    more updates to expressions exercises

--- keywordsExpressions/src/com/company/Main.java
+++ keywordsExpressions/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// an Expression is composed of the variable, value and operators
-    // an Expression is composed of the data type, variable, value, and operators
+    // an Statement is composed of the data type, variable, value, and operators
         double kilometers = (100 * 1.609344);
         System.out.println(kilometers);
     }
