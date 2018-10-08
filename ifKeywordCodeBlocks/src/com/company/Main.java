75920bcb2f89b6a29f6117feca5b33a3e6beacdf
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-08 14:12:00 -0500

    adding more phases of completed exercises to ifKeywordCodeBlocks

--- ifKeywordCodeBlocks/src/com/company/Main.java
+++ ifKeywordCodeBlocks/src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// We use "if" Statements to create a statement to validate certain conditions, and the actions when those conditions are or are not met!
+        boolean gameOver = true;
+        int score = 50000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000){
+            System.out.println("Your score is in the range of 5000!");
+        } else if (score < 1000){
+            System.out.println("Your score is less than 5000!");
+
+        } else {
+            System.out.println("You are here, your score is: " + score);
+        }
+
+        if (gameOver == true){
+            int finalScore = score + (levelCompleted + bonus);
+            System.out.println(finalScore);
+        }
     }
 }
