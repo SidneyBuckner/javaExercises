b5eb8312eb66c0e6857b12dd892d36f10a7ad422
sidneybuckner <sidney.buckner2@gmail.com>
2018-10-09 15:50:44 -0500

    began exercises on Java Methods

--- methods/src/com/company/Main.java
+++ methods/src/com/company/Main.java
@@ -1,8 +1,32 @@
+
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // We use "if" Statements to create a statement to validate certain conditions, and the actions when those conditions are or are not met!
+        boolean gameOver = true;
+        int score = 50000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore();
+
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted + bonus);
+            System.out.println("This is your first final score: " + finalScore);
+        }
+    }
+
+    public static void calculateScore(){
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted + bonus);
+            System.out.println("This is your actual final score: " + finalScore);
+        }
     }
 }
